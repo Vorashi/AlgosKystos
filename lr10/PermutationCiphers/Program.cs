121df55f5ccf3d0be54d58a4c1d037346d4fd46b
Vorashi <matvejsarov673@gmail.com>
2024-06-04 11:29:24 +0300

    Плюс 10 пофиксили

--- lr10/PermutationCiphers/Program.cs
+++ lr10/PermutationCiphers/Program.cs
@@ -14,19 +14,9 @@ namespace PermutationCiphers
         //[STAThread]
         static void Main()
         {
-            PermutationCiphers pc = new PermutationCiphers();
-            //int[] i = { 3, 1, 2 };
-            int[] i = { 3, 0, 2, 1 };
-            int[] j = { 2, 0, 3, 1 };
-            //pc.BlockSingle("финмориатяка", i, out string result);
-            //pc.Route("информационная безопасность", 6, out result);
-            //pc.Vertical("информационная безопасность", "колонна", out result);
-            pc.DoublePermutation("наука математика", i, j, out string result);
-            Console.WriteLine(result);
-            Console.ReadKey();
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
